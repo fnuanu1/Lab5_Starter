9e627e06981d0b842a20a70302a8c633fc3ff454
fnuanu1 <150562704+fnuanu1@users.noreply.github.com>
2025-05-06 13:53:46 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
+
+Team:
+Fnu Anu
+Thanh Hoang
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+   Yes, I would use a unit test to test the "message" feature of a messaging application because I would like to see if the packages have been transported properly to the other end.
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+   Yes, I would use a unit test to test the "max message length" feature of a messaging application because I would like to see if the max length is being followed if the message is being cut short or entirely not delivered.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,123 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+//npm test ./__tests__/unit.test.js
+//2 test true and 2 test false
+
+//phone 1st
+test('adds 1 + 2 to equal 3', () => {
+  // TODO
+  expect(isPhoneNumber(("657) 357-2216"))).toBe(true);
+});
+test('adds 1 + 2 to equal 3', () => {
+  // TODO
+  expect(isPhoneNumber("bro")).toBe(false);
+});
+test('adds 1 + 2 to equal 3', () => {
+  // TODO
+  expect(isPhoneNumber(657)).toBe(false);
+});
+test('adds 1 + 2 to equal 3', () => {
+  // TODO
+  expect(isPhoneNumber("788-657-3527")).toBe(true);
+});
+test('adds 1 + 2 to equal 3', () => {
+  // TODO
+  expect(isPhoneNumber(("657)-357-2216"))).toBe(true);
+});
+test('adds 1 + 2 to equal 3', () => {
+  // TODO
+  expect(isPhoneNumber(("657)357-2216"))).toBe(true);
+});
+
+
+
+
+//2nd
+test('email', () => {
+  // TODO
+  expect(isEmail(("fun"))).toBe(false);
+});
+test('email', () => {
+  // TODO
+  expect(isEmail(("fanu@ucsd.edu"))).toBe(true);
+});
+test('email', () => {
+  // TODO
+  expect(isEmail(("576"))).toBe(false);
+});
+test('email', () => {
+  // TODO
+  expect(isEmail(("fanu@gmail.com"))).toBe(true);
+});
+test('email', () => {
+  // TODO
+  expect(isEmail(("yay@gmail"))).toBe(false);
+});
+
+
+//3rd
+test('strong', () => {
+  // TODO
+  expect(isStrongPassword(("Star7g"))).toBe(true);
+});
+test('strong', () => {
+  // TODO
+  expect(isStrongPassword(("7star"))).toBe(false);
+});
+test('strong', () => {
+  // TODO
+  expect(isStrongPassword(("stg"))).toBe(false);
+});
+test('strong', () => {
+  // TODO
+  expect(isStrongPassword(("$yas"))).toBe(false);
+});
+test('strong', () => {
+  // TODO
+  expect(isStrongPassword(("starN72"))).toBe(true);
+});
+
+
+
+
+//4th
+test('strong', () => {
+  // TODO
+  //XX can be 1 or 2 digits long and YYYY is always 4 digits long.
+  expect(isDate(("23/5/2024"))).toBe(true);
+});
+test('strong', () => {
+  // TODO
+  expect(isDate(("23/5/20245"))).toBe(false);
+});
+test('strong', () => {
+  // TODO
+  expect(isDate(("23052005"))).toBe(false);
+});
+test('strong', () => {
+  // TODO
+  expect(isDate(("2/2/0005"))).toBe(true);
+});
+
+
+
+
+//5th
+//isHexColor
+test('strong', () => {
+  // TODO
+  expect(isHexColor(("FFF"))).toBe(true);
+});
+test('strong', () => {
+  // TODO
+  expect(isHexColor(("#FFFFFF"))).toBe(true);
+});
+test('strong', () => {
+  // TODO
+  expect(isHexColor(("#FFFF"))).toBe(false);
+});
+test('strong', () => {
+  // TODO
+  expect(isHexColor(("#ZZZ"))).toBe(false);
+});
