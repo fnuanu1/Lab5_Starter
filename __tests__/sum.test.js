2c9d495446c94ef910db1ed7a6e4d01f6ba3c95c
fnuanu1 <150562704+fnuanu1@users.noreply.github.com>
2025-05-06 14:54:17 -0700

    testing branch: right test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,9 +3,10 @@ import { sum } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 to equal 3', () => {
   // TODO
-  expect(1 + 2).toBe(6);
+  expect(1 + 2).toBe(3);
 });
 test('sum function', () => {
   // TODO
   expect(sum(1,2)).toBe(3);
 });
+
